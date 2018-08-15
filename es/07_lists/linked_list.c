9cd4634949efeba816fb27bdfaed0e284ce9c678
Alexander Elizarov <al.elizarov@gmail.com>
2018-08-15 22:13:59 +0300

    list: extra operations

--- es/07_lists/linked_list.c
+++ es/07_lists/linked_list.c
@@ -28,9 +28,9 @@ void push_front(int x) {
         head = new_node;
         return;
     }
-    head->prev = new_node;
-    new_node->next = head;
-    head = new_node;
+        head->prev = new_node;
+        new_node->next = head;
+        head = new_node;
     }
 
 
@@ -81,6 +81,34 @@ void print_backward() {
     printf("\n");
 }
 
+/*  Prints head and tail pointers  */
+void print_pointers() {
+    printf("Head: %p \n", head );
+    printf("Head->next : %p \n", head->next );
+    printf("Head->prev : %p \n", head->prev );
+    printf("Head->counter : %d \n", head->counter );
+    printf("Head->next->counter : %d \n", head->next->counter );
+    printf("*******************\n \n");
+
+
+    struct Node* temp = head;
+    if(temp == NULL) return; // empty list, exit
+
+    /* Going to last Node */
+    while(temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    printf("Tail: %p \n", temp );
+    printf("Tail->next : %p \n", temp->next );
+    printf("Tail->prev : %p \n", temp->prev );
+    printf("Tail->counter : %d \n", temp->counter );
+    printf("Tail->prev->counter : %d \n", temp->prev->counter );
+    printf("*******************\n \n");
+
+}
+
+
 int main() {
 
     head = NULL; // empty list. set head as NULL.
@@ -101,6 +129,8 @@ int main() {
     push_front(10);
     print_forward();
     print_backward();
+
+    print_pointers();
     return 0;
 }
 

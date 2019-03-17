04cb53654ac3c786f4540c8e312e4a66451d01f8
Alexander Elizarov <al.elizarov@gmail.com>
2019-03-17 22:46:55 +0200

    es: editorial corrections
    Signed-off-by: Alexander Elizarov <al.elizarov@gmail.com>

--- es/02_simpleCpp/reference.cpp
+++ es/02_simpleCpp/reference.cpp
@@ -4,23 +4,19 @@
 using namespace std ;
  int main()
  {
-   int i = 10 ;
-   int z = 148 ;
-   //const int & ri  = i ;
+    int i = 10 ;
+    int z = 148 ;
     int & ri  = i ;
-   std::cout <<"Ref  to I ,  ri = "<<ri<<std::endl ;
-   i++ ;
-   std::cout <<"After I++ ,  ri = "<<ri<<std::endl ;
+    std::cout <<"Ref  to I ,  ri = "<<ri<<std::endl ;
+    i++ ;
+    std::cout <<"After I++ ,  ri = "<<ri<<std::endl ;
 
    ri = z ;
-
-   std::cout <<"referenced anymore ,   ri = "<<ri<<" I = "<<i<<std::endl ;
-
-   std::cout <<"Orig referenced ,   i = "<<i<<std::endl ;
+   std::cout <<"referenced anymore ,   ri = "<<ri<<" i = "<<i<<std::endl ;
+   std::cout <<"Orig i = "<<i<<std::endl ;
 
 
-   return 1 ;
-
+   return 0;
  }
 
 
